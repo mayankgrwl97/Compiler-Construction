31ab82c4afa81fcc1347d5f674d6f2ae5cc1788e
Mayank Agarwal <mayankgrwl97@gmail.com>
2017-04-20 13:04:04 +0530

    temporary changes

--- codegen.c
+++ codegen.c
@@ -9,6 +9,14 @@
 #include "idsymboltable.h"
 #include "typeExtractor.h"
 
+void endcode()
+{
+	printf("\tmov eax, 1\n");
+	printf("\tmov ebx, 1\n");
+	printf("\tint 80h\n");
+	return;
+}
+
 void initialize(/*FILE* fp, */mainsymboltable* globalTable)
 {
 	printf("section .bss\n");
@@ -30,21 +38,17 @@ void initialize(/*FILE* fp, */mainsymboltable* globalTable)
 
 	printf("\n");
 
-	printf("section .txt\n");
+	printf("section .text\n");
 	printf("\tglobal _start\n\n");
 
 	printf("_start:\n");
 	return;
 }
-/*
-mov eax, 3
-   mov ebx, 2
-   mov ecx, num1  
-   mov edx, 5          ;5 bytes (numeric, 1 for sign) of that information
-   int 80h
-*/
+
 void code_statement(stacknode* temp)
 {
+	if(strcmp(temp->ntortinfo->str, "declareStmt") == 0)
+		return;
 	if(strcmp(temp->ntortinfo->str, "<ioStmt>") == 0)
 	{
 		if(strcmp(temp->child->ntortinfo->str, "GET_VALUE") == 0)
@@ -56,7 +60,22 @@ void code_statement(stacknode* temp)
 			printf("mov ecx, %s\n", pt->idlex);
 			printf("mov edx, %d\n", pt->widthofid);
 			printf("int 80h\n");
+			return;
 		}
-		else if(strcmp)
+		else if(strcmp(temp->child->ntortinfo->str, "PRINT") == 0)
+		{
+			if(strcmp(temp->child->sibling->child->ntortinfo->str, "NUM") == 0)
+			{
+				// print no
+			}
+			else if(strcmp(temp->child->sibling->child->ntortinfo->str, "RNUM") == 0)
+			{
+				// print real
+			}
+			else
+			{
+
+			}
+		}
 	}
 }
