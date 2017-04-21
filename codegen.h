aa02449650e6efc2a927a12b18bb2cddf9ba858c
Mayank Agarwal <mayankgrwl97@gmail.com>
2017-04-21 15:32:53 +0530

    added file pointers in codegen.g

--- codegen.h
+++ codegen.h
@@ -9,13 +9,22 @@
 #include "idsymboltable.h"
 #include "typeExtractor.h"
 
-void endcode();
-void initialize(/*FILE* fp, */mainsymboltable* globalTable);
-void codegenexp(stacknode* curr);
+// void endcode();
+// void initialize(/*FILE* fp, */mainsymboltable* globalTable);
+// void codegenexp(stacknode* curr);
+// int getlabel();
+// void codegeniterative(stacknode* temp);
+// void code_statement(stacknode* temp);
+// void traverseAST_forCodegen(stacknode* curr);
+// void generate_code(mainsymboltable* globalTable, stacknode* astroot);
+void endcode(FILE* fp);
+void initialize(FILE* fp, mainsymboltable* globalTable);
+void codegenexp(FILE* fp, stacknode* curr);
 int getlabel();
-void codegeniterative(stacknode* temp);
-void code_statement(stacknode* temp);
-void traverseAST_forCodegen(stacknode* curr);
-void generate_code(mainsymboltable* globalTable, stacknode* astroot);
+void codegeniterative(FILE* fp, stacknode* temp);
+void codegenconditional(FILE* fp, stacknode* curr);
+void code_statement(FILE* fp, stacknode* temp);
+void traverseAST_forCodegen(FILE* fp, stacknode* curr);
+void generate_code(FILE* fp, mainsymboltable* globalTable, stacknode* astroot);
 
 #endif
