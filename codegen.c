ec2cb605b48b89d1ec527e7cf9edfee4781fb9cf
Mayank Agarwal <mayankgrwl97@gmail.com>
2017-04-21 15:41:58 +0530

    fprintf added in codegen

--- codegen.c
+++ codegen.c
@@ -18,17 +18,17 @@ Karan Deep Batra(2014A7PS160P)
 
 void endcode(FILE* fp)
 {
-	printf("\tmov eax, 1\n");
-	printf("\tmov ebx, 1\n");
-	printf("\tint 80h\n");
-	// printf("\tret\n");
+	fprintf(fp, "\tmov eax, 1\n");
+	fprintf(fp, "\tmov ebx, 1\n");
+	fprintf(fp, "\tint 80h\n");
+	// fprintf(fp, "\tret\n");
 	return;
 }
 
 void initialize(FILE* fp, mainsymboltable* globalTable)
 {
-	printf("SECTION .bss\n");
-	// fprintf(fp, "")
+	fprintf(fp, "SECTION .bss\n");
+	// ffprintf(fp, fp, "")
 	mainsymboltablenode* temp = presentmainsymboltable(globalTable, "program");
 	idsymboltable* pt = temp->idst;
 	for(int i=0; i<idsymboltablesize; i++)
@@ -38,23 +38,23 @@ void initialize(FILE* fp, mainsymboltable* globalTable)
 			idsymboltablenode* curr = pt->buckets[i];
 			while(curr != NULL)
 			{
-				printf("%s\tresb %d\n", curr->idlex, 2*curr->widthofid);
+				fprintf(fp, "%s\tresb %d\n", curr->idlex, 2*curr->widthofid);
 				curr = curr->next;
 			}
 		}
 	}
 
-	printf("\n");
+	fprintf(fp, "\n");
 
-	printf("SECTION .data\n");
-	printf("get_val: db \"%%d\", 0\n");
-	printf("print_val: db \"%%d\", 10, 0\n\n");
-	printf("SECTION .text\n\n");
-	printf("extern printf\n");
-	printf("extern scanf\n\n");
-	printf("GLOBAL main\n\n");
+	fprintf(fp, "SECTION .data\n");
+	fprintf(fp, "get_val: db \"%%d\", 0\n");
+	fprintf(fp, "print_val: db \"%%d\", 10, 0\n\n");
+	fprintf(fp, "SECTION .text\n\n");
+	fprintf(fp, "extern printf\n");
+	fprintf(fp, "extern scanf\n\n");
+	fprintf(fp, "GLOBAL main\n\n");
 
-	printf("main:\n");
+	fprintf(fp, "main:\n");
 	return;
 }
 
@@ -67,8 +67,8 @@ void codegenexp(FILE* fp, stacknode* curr)
 	{
 		if(strcmp(curr->child->ntortinfo->str, "NUM") == 0)
 		{
-			printf("\tmov r8, %s\n", curr->child->tokinfo->lexeme);
-			printf("\tpush r8\n");
+			fprintf(fp, "\tmov r8, %s\n", curr->child->tokinfo->lexeme);
+			fprintf(fp, "\tpush r8\n");
 			return;
 		}
 		else if(strcmp(curr->child->ntortinfo->str, "RNUM") == 0)
@@ -78,9 +78,9 @@ void codegenexp(FILE* fp, stacknode* curr)
 		else
 		{			
 			idsymboltablenode* temp = getidsymboltablenode(curr->child->tokinfo->lexeme, curr->child->idst);
-			printf("\tmov r8, [%s]\n", temp->idlex);
-			printf("\tand r8, 00000000000000ffh\n");
-			printf("\tpush r8\n");
+			fprintf(fp, "\tmov r8, [%s]\n", temp->idlex);
+			fprintf(fp, "\tand r8, 00000000000000ffh\n");
+			fprintf(fp, "\tpush r8\n");
 		}
 	}
 
@@ -89,19 +89,19 @@ void codegenexp(FILE* fp, stacknode* curr)
 		if(curr->sibling != NULL)
 		{
 			codegenexp(fp, curr->sibling);
-			printf("\tpop r9\n");
-			printf("\tmov r8, 0\n");
-			printf("\tsub r8, r9\n");
-			printf("\tpush r8\n");
+			fprintf(fp, "\tpop r9\n");
+			fprintf(fp, "\tmov r8, 0\n");
+			fprintf(fp, "\tsub r8, r9\n");
+			fprintf(fp, "\tpush r8\n");
 		}
 		else
 		{
 			codegenexp(fp, curr->child);
 			codegenexp(fp, curr->child->sibling);
-			printf("\tpop r9\n");
-			printf("\tpop r8\n");
-			printf("\tsub r8,r9\n");
-			printf("\tpush r8\n");
+			fprintf(fp, "\tpop r9\n");
+			fprintf(fp, "\tpop r8\n");
+			fprintf(fp, "\tsub r8,r9\n");
+			fprintf(fp, "\tpush r8\n");
 		}
 	}
 
@@ -109,30 +109,30 @@ void codegenexp(FILE* fp, stacknode* curr)
 	{
 		codegenexp(fp, curr->child);
 		codegenexp(fp, curr->child->sibling);
-		printf("\tpop r9\n");
-		printf("\tpop r8\n");
-		printf("\tadd r8,r9\n");
-		printf("\tpush r8\n");
+		fprintf(fp, "\tpop r9\n");
+		fprintf(fp, "\tpop r8\n");
+		fprintf(fp, "\tadd r8,r9\n");
+		fprintf(fp, "\tpush r8\n");
 	}
 
 	if(strcmp(curr->ntortinfo->str, "MUL") == 0)
 	{
 		codegenexp(fp, curr->child);
 		codegenexp(fp, curr->child->sibling);
-		printf("\tpop r9\n");
-		printf("\tpop r8\n");
-		printf("\timul r8,r9\n");
-		printf("\tpush r8\n");
+		fprintf(fp, "\tpop r9\n");
+		fprintf(fp, "\tpop r8\n");
+		fprintf(fp, "\timul r8,r9\n");
+		fprintf(fp, "\tpush r8\n");
 	}
 
 	if(strcmp(curr->ntortinfo->str, "DIV") == 0)
 	{
 		codegenexp(fp, curr->child);
 		codegenexp(fp, curr->child->sibling);
-		printf("\tpop r9\n");
-		printf("\tpop r8\n");
-		printf("\tdiv r8,r9\n");
-		printf("\tpush r8\n");
+		fprintf(fp, "\tpop r9\n");
+		fprintf(fp, "\tpop r8\n");
+		fprintf(fp, "\tdiv r8,r9\n");
+		fprintf(fp, "\tpush r8\n");
 	}
 
 	if((strcmp(curr->ntortinfo->str, "LT") == 0) || (strcmp(curr->ntortinfo->str, "LE") == 0) || (strcmp(curr->ntortinfo->str, "GT") == 0) || (strcmp(curr->ntortinfo->str, "GE") == 0) || (strcmp(curr->ntortinfo->str, "EQ") == 0) || (strcmp(curr->ntortinfo->str, "NE") == 0))
@@ -141,80 +141,80 @@ void codegenexp(FILE* fp, stacknode* curr)
 		codegenexp(fp, curr->child->sibling);
 		int truelabel = getlabel();
 		int endlabel = getlabel();
-		printf("\tpop r9\n");
-		printf("\tpop r8\n");
-		printf("\tcmp r8,r9\n");
+		fprintf(fp, "\tpop r9\n");
+		fprintf(fp, "\tpop r8\n");
+		fprintf(fp, "\tcmp r8,r9\n");
 		if(strcmp(curr->ntortinfo->str, "LT") == 0)
-			printf("\tjl truelabel_%d\n", truelabel);
+			fprintf(fp, "\tjl truelabel_%d\n", truelabel);
 		else if(strcmp(curr->ntortinfo->str, "LE") == 0)
-			printf("\tjle truelabel_%d\n", truelabel);
+			fprintf(fp, "\tjle truelabel_%d\n", truelabel);
 		else if(strcmp(curr->ntortinfo->str, "GT") == 0)
-			printf("\tjg truelabel_%d\n", truelabel);
+			fprintf(fp, "\tjg truelabel_%d\n", truelabel);
 		else if(strcmp(curr->ntortinfo->str, "GE") == 0)
-			printf("\tjge truelabel_%d\n", truelabel);
+			fprintf(fp, "\tjge truelabel_%d\n", truelabel);
 		else if(strcmp(curr->ntortinfo->str, "EQ") == 0)
-			printf("\tje truelabel_%d\n", truelabel);
+			fprintf(fp, "\tje truelabel_%d\n", truelabel);
 		else if(strcmp(curr->ntortinfo->str, "NEQ") == 0)
-			printf("\tjne truelabel_%d\n", truelabel);
-		printf("\tmov r8, 0\n");
-		printf("\tpush r8\n");
-		printf("\tjmp label_%d\n", endlabel);
-		printf("truelabel_%d:\n", truelabel);
-		printf("\tmov r8, 1\n");
-		printf("\tpush r8\n");
-		printf("label_%d:\n", endlabel);
+			fprintf(fp, "\tjne truelabel_%d\n", truelabel);
+		fprintf(fp, "\tmov r8, 0\n");
+		fprintf(fp, "\tpush r8\n");
+		fprintf(fp, "\tjmp label_%d\n", endlabel);
+		fprintf(fp, "truelabel_%d:\n", truelabel);
+		fprintf(fp, "\tmov r8, 1\n");
+		fprintf(fp, "\tpush r8\n");
+		fprintf(fp, "label_%d:\n", endlabel);
 	}
 
 	if(strcmp(curr->ntortinfo->str, "AND") == 0)
 	{
 		codegenexp(fp, curr->child);
 		codegenexp(fp, curr->child->sibling);
-		printf("\tpop r9\n");
-		printf("\tpop r8\n");
+		fprintf(fp, "\tpop r9\n");
+		fprintf(fp, "\tpop r8\n");
 		int firsttruelabel = getlabel();
 		int secondfalselabel = getlabel();
 		int endlabel = getlabel();
-		printf("\tcmp r8, 0000000000000001h\n");
-		printf("\tje label_%d\n", firsttruelabel);
-		printf("\tmov r8, 0000000000000000h\n");
-		printf("\tpush r8\n");
-		printf("\tjmp label_%d\n", endlabel);
-		printf("label_%d:\n", firsttruelabel);
-		printf("\tcmp r9, 0000000000000001h\n");
-		printf("\tjne label_%d\n", secondfalselabel);
-		printf("\tmov r8, 0000000000000001h\n");
-		printf("\tpush r8\n");
-		printf("\tjmp label_%d\n", endlabel);
-		printf("label_%d:\n",secondfalselabel);
-		printf("\tmov r8, 0000000000000000h\n");
-		printf("\tpush r8\n");
-		printf("label_%d:\n",endlabel);
+		fprintf(fp, "\tcmp r8, 0000000000000001h\n");
+		fprintf(fp, "\tje label_%d\n", firsttruelabel);
+		fprintf(fp, "\tmov r8, 0000000000000000h\n");
+		fprintf(fp, "\tpush r8\n");
+		fprintf(fp, "\tjmp label_%d\n", endlabel);
+		fprintf(fp, "label_%d:\n", firsttruelabel);
+		fprintf(fp, "\tcmp r9, 0000000000000001h\n");
+		fprintf(fp, "\tjne label_%d\n", secondfalselabel);
+		fprintf(fp, "\tmov r8, 0000000000000001h\n");
+		fprintf(fp, "\tpush r8\n");
+		fprintf(fp, "\tjmp label_%d\n", endlabel);
+		fprintf(fp, "label_%d:\n",secondfalselabel);
+		fprintf(fp, "\tmov r8, 0000000000000000h\n");
+		fprintf(fp, "\tpush r8\n");
+		fprintf(fp, "label_%d:\n",endlabel);
 	}
 
 	if(strcmp(curr->ntortinfo->str, "OR") == 0)
 	{
 		codegenexp(fp, curr->child);
 		codegenexp(fp, curr->child->sibling);
-		printf("\tpop r9\n");
-		printf("\tpop r8\n");
+		fprintf(fp, "\tpop r9\n");
+		fprintf(fp, "\tpop r8\n");
 		int firstfalselabel = getlabel();
 		int secondtruelabel = getlabel();
 		int endlabel = getlabel();
-		printf("\tcmp r8, 0000000000000001h\n");
-		printf("\tjne label_%d\n", firstfalselabel);
-		printf("\tmov r8, 0000000000000001h\n");
-		printf("\tpush r8\n");
-		printf("\tjmp label_%d\n", endlabel);
-		printf("label_%d:\n", firstfalselabel);
-		printf("\tcmp r9, 0000000000000001h\n");
-		printf("\tje label_%d\n", secondtruelabel);
-		printf("\tmov r8, 0000000000000000h\n");
-		printf("\tpush r8\n");
-		printf("\tjmp label_%d\n", endlabel);
-		printf("label_%d:\n",secondtruelabel);
-		printf("\tmov r8, 0000000000000001h\n");
-		printf("\tpush r8\n");
-		printf("label_%d:\n",endlabel);
+		fprintf(fp, "\tcmp r8, 0000000000000001h\n");
+		fprintf(fp, "\tjne label_%d\n", firstfalselabel);
+		fprintf(fp, "\tmov r8, 0000000000000001h\n");
+		fprintf(fp, "\tpush r8\n");
+		fprintf(fp, "\tjmp label_%d\n", endlabel);
+		fprintf(fp, "label_%d:\n", firstfalselabel);
+		fprintf(fp, "\tcmp r9, 0000000000000001h\n");
+		fprintf(fp, "\tje label_%d\n", secondtruelabel);
+		fprintf(fp, "\tmov r8, 0000000000000000h\n");
+		fprintf(fp, "\tpush r8\n");
+		fprintf(fp, "\tjmp label_%d\n", endlabel);
+		fprintf(fp, "label_%d:\n",secondtruelabel);
+		fprintf(fp, "\tmov r8, 0000000000000001h\n");
+		fprintf(fp, "\tpush r8\n");
+		fprintf(fp, "label_%d:\n",endlabel);
 	}
 	return;
 }
@@ -229,17 +229,17 @@ void codegeniterative(FILE* fp, stacknode* temp)
 {
 	if(strcmp(temp->child->ntortinfo->str, "FOR") == 0)
 	{
-		printf("\tmov r8, %s\n", temp->child->sibling->sibling->child->tokinfo->lexeme);
-		printf("\tpush r8\n");
+		fprintf(fp, "\tmov r8, %s\n", temp->child->sibling->sibling->child->tokinfo->lexeme);
+		fprintf(fp, "\tpush r8\n");
 		int startlabel = getlabel();
 		int endlabel = getlabel();
-		printf("label_%d:\n", startlabel);
-		printf("\tpop r8\n");
-		printf("\tpush r8\n");
-		printf("\tmov [%s], r8w\n", temp->child->sibling->tokinfo->lexeme);
-		printf("\tmov r9, %s\n", temp->child->sibling->sibling->child->sibling->tokinfo->lexeme);
-		printf("\tcmp r8, r9\n");
-		printf("\tjg label_%d\n", endlabel);
+		fprintf(fp, "label_%d:\n", startlabel);
+		fprintf(fp, "\tpop r8\n");
+		fprintf(fp, "\tpush r8\n");
+		fprintf(fp, "\tmov [%s], r8w\n", temp->child->sibling->tokinfo->lexeme);
+		fprintf(fp, "\tmov r9, %s\n", temp->child->sibling->sibling->child->sibling->tokinfo->lexeme);
+		fprintf(fp, "\tcmp r8, r9\n");
+		fprintf(fp, "\tjg label_%d\n", endlabel);
 
 		stacknode* temp2 = temp->child->sibling->sibling->sibling->sibling->child;
 		while(temp2 != NULL)
@@ -248,30 +248,30 @@ void codegeniterative(FILE* fp, stacknode* temp)
 			temp2 = temp2->sibling;
 		}
 
-		printf("\tpop r8\n");
-		printf("\tinc r8\n");
-		printf("\tpush r8\n");
-		printf("\tjmp label_%d\n", startlabel);
-		printf("label_%d: \n", endlabel);
+		fprintf(fp, "\tpop r8\n");
+		fprintf(fp, "\tinc r8\n");
+		fprintf(fp, "\tpush r8\n");
+		fprintf(fp, "\tjmp label_%d\n", startlabel);
+		fprintf(fp, "label_%d: \n", endlabel);
 	}
 	if(strcmp(temp->child->ntortinfo->str, "WHILE") == 0)
 	{
 		int startlabel = getlabel();
 		int endlabel = getlabel();
 
-		printf("label_%d:\n", startlabel);	
+		fprintf(fp, "label_%d:\n", startlabel);	
 		codegenexp(fp, temp->child->sibling);
-		printf("\tpop r8\n");
-		printf("\tcmp r8, 0000000000000001h\n");
-		printf("\tjne label_%d\n", endlabel);
+		fprintf(fp, "\tpop r8\n");
+		fprintf(fp, "\tcmp r8, 0000000000000001h\n");
+		fprintf(fp, "\tjne label_%d\n", endlabel);
 		stacknode* temp2 = temp->child->sibling->sibling->sibling->child;
 		while(temp2 != NULL)
 		{
 			code_statement(fp, temp2);
 			temp2 = temp2->sibling;
 		}
-		printf("\tjmp label_%d\n", startlabel);
-		printf("label_%d:\n", endlabel);
+		fprintf(fp, "\tjmp label_%d\n", startlabel);
+		fprintf(fp, "label_%d:\n", endlabel);
 	}
 	return;
 }
@@ -281,12 +281,12 @@ void codegenconditional(FILE* fp, stacknode* curr)
 	idsymboltable* idst = curr->child->idst;
 	idsymboltablenode* pt = getidsymboltablenode(curr->child->tokinfo->lexeme, idst);
 
-	printf("\tmov r8, [%s]\n", pt->idlex);
+	fprintf(fp, "\tmov r8, [%s]\n", pt->idlex);
 
 	if(gettype(pt->type) == integer)
 	{
-		printf("\tand r8, 00000000000000ffh\n");
-		printf("\tpush r8\n");
+		fprintf(fp, "\tand r8, 00000000000000ffh\n");
+		fprintf(fp, "\tpush r8\n");
 
 		stacknode* temp = curr->child->sibling->sibling; //<caseStmts>
 		stacknode* temp2 = temp->child;
@@ -296,36 +296,36 @@ void codegenconditional(FILE* fp, stacknode* curr)
 		while(temp2 != NULL)
 		{
 			int nextlabel = getlabel();
-			printf("label_%d:\n", currlabel);
-			printf("\tpop r8\n");
-			printf("\tpush r8\n");
-			printf("\tmov r9, %s\n", temp2->child->tokinfo->lexeme);
-			printf("\tcmp r8,r9\n");
-			printf("\tjne label_%d\n", nextlabel);
+			fprintf(fp, "label_%d:\n", currlabel);
+			fprintf(fp, "\tpop r8\n");
+			fprintf(fp, "\tpush r8\n");
+			fprintf(fp, "\tmov r9, %s\n", temp2->child->tokinfo->lexeme);
+			fprintf(fp, "\tcmp r8,r9\n");
+			fprintf(fp, "\tjne label_%d\n", nextlabel);
 			stacknode* temp3 = temp2->sibling->child;
 			while(temp3 != NULL)
 			{
 				code_statement(fp, temp3);
 				temp3 = temp3->sibling;
 			}
-			printf("\tjmp label_%d\n", endlabel);
+			fprintf(fp, "\tjmp label_%d\n", endlabel);
 			currlabel = nextlabel;
 			temp2 = temp2->sibling->sibling;
 		}
 		temp2 = temp->sibling->child->child;
-		printf("label_%d:\n", currlabel);
+		fprintf(fp, "label_%d:\n", currlabel);
 		while(temp2 != NULL)
 		{
 			code_statement(fp, temp2);
 			temp2 = temp2->sibling;
 		}
 
-		printf("label_%d:\n", endlabel);
+		fprintf(fp, "label_%d:\n", endlabel);
 	}
 	else
 	{
-		printf("\tand r8, 0000000000000001h\n");
-		printf("\tpush r8\n");
+		fprintf(fp, "\tand r8, 0000000000000001h\n");
+		fprintf(fp, "\tpush r8\n");
 
 		stacknode* temp = curr->child->sibling->sibling;
 		stacknode* temp2 = temp->child;
@@ -336,27 +336,27 @@ void codegenconditional(FILE* fp, stacknode* curr)
 		while(temp2 != NULL)
 		{
 			nextlabel = getlabel();
-			printf("label_%d:\n", currlabel);
-			printf("\tpop r8\n"); //pop id from stack
-			printf("\tpush r8\n"); //push id to stack
+			fprintf(fp, "label_%d:\n", currlabel);
+			fprintf(fp, "\tpop r8\n"); //pop id from stack
+			fprintf(fp, "\tpush r8\n"); //push id to stack
 			if(strcmp(temp2->child->tokinfo->lexeme, "true") == 0)
-				printf("\tmov r9, 0000000000000001h\n");
+				fprintf(fp, "\tmov r9, 0000000000000001h\n");
 			else
-				printf("\tmov r9, 0000000000000000h\n");
-			printf("\tcmp r8,r9\n");
-			printf("\tjne label_%d\n", nextlabel);
+				fprintf(fp, "\tmov r9, 0000000000000000h\n");
+			fprintf(fp, "\tcmp r8,r9\n");
+			fprintf(fp, "\tjne label_%d\n", nextlabel);
 			stacknode* temp3 = temp2->sibling->child;
 			while(temp3 != NULL)
 			{
 				code_statement(fp, temp3);
 				temp3 = temp3->sibling;
 			}
-			printf("\tjmp label_%d\n",endlabel);
+			fprintf(fp, "\tjmp label_%d\n",endlabel);
 			currlabel = nextlabel;
 			temp2 = temp2->sibling->sibling;
 		}
-		printf("label_%d:\n", nextlabel);
-		printf("label_%d:\n", endlabel);
+		fprintf(fp, "label_%d:\n", nextlabel);
+		fprintf(fp, "label_%d:\n", endlabel);
 	}
 	return;
 }
@@ -376,11 +376,11 @@ void code_statement(FILE* fp, stacknode* temp)
 			// idsymboltable* idst = temp->child->sibling->idst;
 			// idsymboltablenode* pt = getidsymboltablenode(temp->child->sibling->tokinfo->lexeme, idst);
 
-			printf("\tmov rdi, get_val\n");
-			printf("\tlea rsi, [%s]\n", temp->child->sibling->tokinfo->lexeme);
-			printf("\tmov al, 0\n");
-			printf("\tcall scanf\n");
-			printf("\n");
+			fprintf(fp, "\tmov rdi, get_val\n");
+			fprintf(fp, "\tlea rsi, [%s]\n", temp->child->sibling->tokinfo->lexeme);
+			fprintf(fp, "\tmov al, 0\n");
+			fprintf(fp, "\tcall scanf\n");
+			fprintf(fp, "\n");
 			return;
 		}
 		else if(strcmp(temp->child->ntortinfo->str, "PRINT") == 0)
@@ -388,26 +388,26 @@ void code_statement(FILE* fp, stacknode* temp)
 			stacknode* temp2 = temp->child->sibling->child;
 			if(strcmp(temp2->ntortinfo->str, "NUM") == 0)
 			{
-				printf("\tmov rdi, print_val\n");
-				printf("\tmov rsi, %s\n", temp2->tokinfo->lexeme);
-				printf("\tmov al, 0\n");
-				printf("\tcall printf\n");
-				printf("\n");
+				fprintf(fp, "\tmov rdi, print_val\n");
+				fprintf(fp, "\tmov rsi, %s\n", temp2->tokinfo->lexeme);
+				fprintf(fp, "\tmov al, 0\n");
+				fprintf(fp, "\tcall printf\n");
+				fprintf(fp, "\n");
 			}
 			else
 			{
 				idsymboltable* idst = temp2->idst;
 				idsymboltablenode* pt = getidsymboltablenode(temp2->tokinfo->lexeme, idst);
-				printf("\tmov rdi, print_val\n");
-				printf("\tmov rax, [%s]\n", temp2->tokinfo->lexeme);
+				fprintf(fp, "\tmov rdi, print_val\n");
+				fprintf(fp, "\tmov rax, [%s]\n", temp2->tokinfo->lexeme);
 				if(pt->widthofid == 2)
-					printf("\tand rax, 00000000000000ffh\n");
+					fprintf(fp, "\tand rax, 00000000000000ffh\n");
 				else
-					printf("\tand rax, 000000000000000fh\n");
-				printf("\tmov rsi, rax\n");
-				printf("\tmov al, 0\n");
-				printf("\tcall printf\n");
-				printf("\n");
+					fprintf(fp, "\tand rax, 000000000000000fh\n");
+				fprintf(fp, "\tmov rsi, rax\n");
+				fprintf(fp, "\tmov al, 0\n");
+				fprintf(fp, "\tcall printf\n");
+				fprintf(fp, "\n");
 			}
 		}
 	}
@@ -417,13 +417,13 @@ void code_statement(FILE* fp, stacknode* temp)
 		if(strcmp(temp->child->sibling->ntortinfo->str, "<lvalueIDStmt>") == 0)
 		{
 			codegenexp(fp, temp->child->sibling->child);
-			printf("\tpop r8\n");
+			fprintf(fp, "\tpop r8\n");
 			idsymboltable* idst = temp->child->idst;
 			idsymboltablenode* pt = getidsymboltablenode(temp->child->tokinfo->lexeme, idst);
 			if(pt->widthofid == 2)
-				printf("\tmov [%s], r8w\n", temp->child->tokinfo->lexeme);//for integers
+				fprintf(fp, "\tmov [%s], r8w\n", temp->child->tokinfo->lexeme);//for integers
 			else
-				printf("\tmov [%s], r8b\n", temp->child->tokinfo->lexeme);//for booleans
+				fprintf(fp, "\tmov [%s], r8b\n", temp->child->tokinfo->lexeme);//for booleans
 		}
 	}
 
